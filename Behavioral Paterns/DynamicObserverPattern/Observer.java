a4a5a1390855b5b892b91259fb353eea985fb5b0
Haj Wali <hajwalii3@gmail.com>
2025-03-23 13:08:26 +0500

    changes

--- Behavioral Paterns/DynamicObserverPattern/Observer.java
+++
@@ -1,7 +0,0 @@
-package DynamicObserverPattern;
-
-public abstract class Observer {
-    protected Subject subject;
-
-    public abstract void update();
-}
--- Behavioral Paterns/EventManagement/EventListener.java
+++
@@ -1,7 +0,0 @@
-package EventManagement;
-
-import java.io.File;
-
-public interface EventListener {
-    void update(String eventType, File file);
-}
